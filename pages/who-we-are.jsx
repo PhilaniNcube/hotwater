94b7ef7ca693b05ac2b0b27b388115632bb0f6ce
Philani Ncube <ncbphi001@gmail.com>
2022-02-13 09:23:50 +0200

    why us page

--- pages/who-we-are.jsx
+++ pages/who-we-are.jsx
@@ -1,95 +1,77 @@
-/* eslint-disable @next/next/no-img-element */
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const About = () => {
+const WhoWeAre = () => {
   return (
-    <div className="px-4 py-9 md:py-12 md:px-6 lg:py-16 lg:px-20">
-      <div className="grid lg:grid-flow-row lg:grid-cols-2 lg:gap-6 ">
-        <h1 className="text-center md:text-left lg:text-5xl md:text-4xl text-2xl font-bold leading-10 text-gray-800 pb-4">
-          About Us
-        </h1>
-        <p className=" text-center md:text-left text-base leading-normal text-gray-600">
-          Generating random paragraphs can be an excellent way for writers to
-          get their creative flow going at the beginning of the day. The writer
-          has no idea what topic the random paragraph will be about when it
-          appears.{' '}
-        </p>
-      </div>
-      <div className="md:px-24 lg:px-0 place-items-center pt-8 pb-8 md:pb-10 lg:pb-14 grid lg:items-end md:grid-flow-row md:grid-cols-2 lg:grid-cols-4 gap-6">
-        <img
-          className="hidden lg:block"
-          src="https://i.ibb.co/8xPszDj/Rectangle-105.png"
-          alt="a guy in glasses"
-        />
-        <img
-          className="hidden lg:block"
-          src="https://i.ibb.co/LS0jnRY/Rectangle-110.png"
-          alt="a guy sitting on a chair"
-        />
-        <img
-          className="hidden lg:block"
-          src="https://i.ibb.co/N2cFW2t/Rectangle-109.png"
-          alt="a guy looking outside a window"
-        />
-        <img
-          className="hidden lg:block"
-          src="https://i.ibb.co/zPqXjgM/Rectangle-111.png"
-          alt="a guy looking side ways"
-        />
-
-        <img
-          className="lg:hidden"
-          src="https://i.ibb.co/r5m9cmb/Rectangle-105-1.png"
-          alt="a guy in glasses"
-        />
-        <img
-          className="lg:hidden"
-          src="https://i.ibb.co/xXPjCnL/Rectangle-110-1.png"
-          alt="a guy sitting on a chair "
-        />
-        <img
-          className="lg:hidden"
-          src="https://i.ibb.co/Z1b4Ssg/Rectangle-109-1.png"
-          alt="a guy looking outside a window "
-        />
-        <img
-          className="lg:hidden"
-          src="https://i.ibb.co/zn0JYBv/Rectangle-111-1.png"
-          alt="a guy looking sideways "
-        />
-      </div>
-      <div className="grid lg:grid-flow-row lg:grid-cols-2 gap-6 md:gap-8 lg:gap-6">
-        <div>
-          <h1 className="text-center md:text-left lg:text-5xl md:text-4xl text-2xl font-bold leading-10 text-gray-800 lg:pb-6 pb-4">
-            Our Mission
+    <Fragment>
+      <header
+        className="bg-center bg-cover bg-no-repeat"
+        style={{ backgroundImage: 'url(/images/hot-shower.jpg)' }}
+      >
+        <div className="max-w-6xl mx-auto my-auto py-10 px-4 lg:px-0">
+          <h1 className="text-white font-light text-3xl lg:text-5xl">
+            About <span className="font-bold">Us</span>
           </h1>
-          <p className=" text-center md:text-left text-base leading-normal text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magnis elit
-            adipiscing mattis tincidunt bibendum gravida eu non libero. Pretium
-            nunc, justo, malesuada ac. Feugiat turpis lobortis interdum non vel.
-            Eu sollicitudin iaculis tincidunt sed imperdiet volutpat.{' '}
+
+          <p className="text-sm max-w-[55ch] text-white pb-2">
+            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
+            sunt labore pratur am dolendunda volorep repudae qui rem.
+            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
+            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
+            preped eaque prore dolorru ndent, est, antur, venim non parchic
+            ipicipsant. Ecerferum rem aut qui des volorum rem re suscima num
+            commollicia quam venis se et velesti onsenem
+          </p>
+          <p className="text-sm max-w-[55ch] text-white">
+            Oviti nullorem et, ut qui il et facearum rae volorehent endem fugia
+            quas non ped magnim exceris est eliquiasit inctese quatur? Por modi
+            dolo dipita dit debitio tori oditia id excesti ure
           </p>
         </div>
-        <div className="flex justify-center items-center">
-          <img
-            className="hidden lg:block xl:hidden"
-            src="https://i.ibb.co/9220t1S/Rectangle-115.png"
-            alt="a guy"
-          />
-          <img
-            className="hidden sm:block lg:hidden xl:block "
-            src="https://i.ibb.co/FxT529H/Rectangle-115-1.png"
-            alt="a guy"
-          />
-          <img
-            className="sm:hidden"
-            src="https://i.ibb.co/xXPjCnL/Rectangle-110-1.png"
-            alt="a guy sitting on a chair"
-          />
+      </header>
+      <section className="max-w-6xl mx-auto py-10 px-4 lg:px-0 grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="text-center">
+          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
+            Our <span className="font-bold">Mission</span>
+          </h2>
+          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
+            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
+            sunt labore pratur am dolendunda volorep repudae qui rem.
+            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
+            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
+            preped eaque prore dolorru ndent, est, antur, venim non parchic
+            ipicipsant. Ecerferum rem aut qui des volorum rem re suscima num
+            commollicia quam venis se et velesti onsenem
+          </p>
         </div>
-      </div>
-    </div>
+        <div className="text-center">
+          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
+            Our <span className="font-bold">Vision</span>
+          </h2>
+          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
+            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
+            sunt labore pratur am dolendunda volorep repudae qui rem.
+            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
+            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
+            preped
+          </p>
+        </div>
+        <div className="text-center">
+          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
+            Our <span className="font-bold">Values</span>
+          </h2>
+          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
+            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
+            sunt labore pratur am dolendunda volorep repudae qui rem.
+            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
+            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
+            preped eaque prore dolorru ndent, est, antur, venim non parchic
+            ipicipsant. Ecerferum rem aut qui des volorum rem re suscima num
+            commollicia quam venis se et velesti onsenem
+          </p>
+        </div>
+      </section>
+    </Fragment>
   );
 };
 
-export default About;
+export default WhoWeAre;
