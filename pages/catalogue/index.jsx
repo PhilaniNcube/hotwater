315e9fd72ff9de2447fe5a2a60594e099bf94590
Philani Ncube <ncbphi001@gmail.com>
2022-02-22 00:02:49 +0200

    catalogue no products

--- pages/catalogue/index.jsx
+++ pages/catalogue/index.jsx
@@ -5,7 +5,7 @@ import CatalogueHero from '../../components/Catalogue/CatalogueHero';
 import CatalogueGrid from '../../components/Catalogue/CatalogueGrid';
 import { useProducts } from '../../hooks/products';
 
-const Catalogue = ({ flowRate }) => {
+const Catalogue = ({ flowRate = 0 }) => {
   const flow = Math.ceil(flowRate);
 
   const {
@@ -26,32 +26,8 @@ const Catalogue = ({ flowRate }) => {
   return (
     <Fragment>
       <CatalogueHero />
-      <div className="flex justify-center text-center py-4">
-        <p className="text-xl font-bold">
-          Based on your answers we rcommend a geyser with a minimum flow rate of{' '}
-          {`${flow} Litres/minute`}
-        </p>
-      </div>
-      {!!products.products && (
-        <div className="flex justify-center text-center max-w-3xl mx-auto mt-4 bg-slate-100 shadow-lg rounded p-8">
-          <p>
-            We currently do not have geysers with a flow rate greater than{' '}
-            {`${flow} Litres/minute`}. We will be in touch with alternative
-            optiins for you
-          </p>
-        </div>
-      )}
-      {!!products.products && <CatalogueGrid products={products.products} />}
     </Fragment>
   );
 };
 
 export default Catalogue;
-
-export async function getServerSideProps({ query: { flowRate = 0 } }) {
-  return {
-    props: {
-      flowRate,
-    },
-  };
-}
