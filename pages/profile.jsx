731ff5493e178c5c4a686fe0f99ea63ea9ac5702
Philani Ncube <ncbphi001@gmail.com>
2022-02-21 23:16:38 +0200

    profile page update

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { supabase } from '../client';
-import useAuth from '../hooks/useAuth';
 
 const Profile = () => {
   return <div className="max-w-6xl mx-auto my-auto bg-gray-200 rounded"></div>;
