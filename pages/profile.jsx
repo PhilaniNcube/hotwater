1621f747508d6120b52f77e67fe768fe1a28b6ca
Philani Ncube <ncbphi001@gmail.com>
2022-02-26 21:30:14 +0200

    profile page

--- pages/profile.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-
-const Profile = () => {
-  return <div className="max-w-6xl mx-auto my-auto bg-gray-200 rounded"></div>;
-};
-
-export default Profile;
---
+++ pages/profile/index.jsx
@@ -0,0 +1,7 @@
+import ProfileNav from '../../components/Profile/ProfileSidebar';
+
+const Profile = () => {
+  return <ProfileNav></ProfileNav>;
+};
+
+export default Profile;
