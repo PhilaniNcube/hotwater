55d8b3dff28f913ed204d302d4978be9cc7c23ac
Philani Ncube <ncbphi001@gmail>
2022-03-03 11:38:24 +0200

    who we are

--- pages/who-we-are.jsx
+++ pages/who-we-are.jsx
@@ -33,48 +33,6 @@ const WhoWeAre = () => {
           </p>
         </div>
       </header>
-      <section className="max-w-6xl mx-auto py-10 px-4 lg:px-0 grid grid-cols-1 lg:grid-cols-3 gap-4">
-        <div className="text-center">
-          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
-            Our <span className="font-bold">Mission</span>
-          </h2>
-          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
-            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
-            sunt labore pratur am dolendunda volorep repudae qui rem.
-            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
-            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
-            preped eaque prore dolorru ndent, est, antur, venim non parchic
-            ipicipsant. Ecerferum rem aut qui des volorum rem re suscima num
-            commollicia quam venis se et velesti onsenem
-          </p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
-            Our <span className="font-bold">Vision</span>
-          </h2>
-          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
-            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
-            sunt labore pratur am dolendunda volorep repudae qui rem.
-            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
-            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
-            preped
-          </p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-gray-600 font-light text-3xl lg:text-5xl">
-            Our <span className="font-bold">Values</span>
-          </h2>
-          <p className="text-sm max-w-[55ch] text-gray-600 pb-2">
-            Fugiandam, ulluptas dit late et volliquas et, quae vendelent quiate
-            sunt labore pratur am dolendunda volorep repudae qui rem.
-            Cessuscimpos et alibus destem et, nisquo ommo eseque nus, iuntio.
-            Tatur senim aliande modionsenem si berum aut fugitatas accupta que
-            preped eaque prore dolorru ndent, est, antur, venim non parchic
-            ipicipsant. Ecerferum rem aut qui des volorum rem re suscima num
-            commollicia quam venis se et velesti onsenem
-          </p>
-        </div>
-      </section>
     </Fragment>
   );
 };
