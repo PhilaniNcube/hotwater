c30ad3b578703b4c91d130e6cd253aee7d534278
Philani Ncube <ncbphi001@gmail.com>
2022-03-05 21:49:27 +0200

    /api/auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -3,10 +3,8 @@ import cookie from 'cookie';
 
 export default async function handler(req, res) {
   await supabase.auth.api.setAuthCookie(req, res);
-
   const token = cookie.parse(req.headers.cookie)['sb:token'];
 
   supabase.auth.session = () => ({ access_token: token });
-
   const { user } = await supabase.auth.api.getUserByCookie(req);
 }
