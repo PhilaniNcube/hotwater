9e7bba6be6f633d42384bda905e20bbdd853dcb5
Philani Ncube <ncbphi001@gmail.com>
2022-04-02 10:13:52 +0200

    st

--- components/QuoteSteps/Step9.jsx
+++ components/QuoteSteps/Step9.jsx
@@ -5,7 +5,7 @@ import { supabase } from '../../utils/supabase';
 import ProductSlide from '../ProductSlide/ProductSlide';
 
 const Step9 = ({ quoteInfo, nextPage, prevPage, page, setQuoteInfo }) => {
-  cosole.log('Step9', quoteInfo);
+  console.log('Step9', quoteInfo);
 
   const [products, setProducts] = useState([]);
   const flowRate = Math.ceil(parseInt(quoteInfo.flowRate + 1));
