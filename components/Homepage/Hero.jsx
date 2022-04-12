f874c587b36ca3259847d5f3e38f4236818e5881
Philani Ncube <ncbphi001@gmail.com>
2022-04-12 20:13:40 +0200

    image quality

--- components/Homepage/Hero.jsx
+++ components/Homepage/Hero.jsx
@@ -19,15 +19,17 @@ const Hero = () => {
       </div>
       <div className="max-w-7xl flex flex-col space-y-3 justify-center aspect-video min-h-[70vh] mx-auto bg-cover z-20 bg-center bg-no-repeat px-4 lg:px-16 py-12">
         <h1 className="text-2xl lg:text-4xl font-sans font-bold mb-3 text-white">
-          Start saving up to <span className="lg:text-6xl ">20%</span> <br />
-          on electricity bill
+          Ave up to <span className="lg:text-6xl ">20%</span> <br />
+          on electricity bill.
         </h1>
         <p className="text-md lg:text-xl mb-4 font-medium leading-3 text-white">
           Say goodbye to cold showers, <br />
-          get a gas geyser installation for your home.
+          get a gas geyser installation
+          <br />
+          for your home.
         </p>
         <Link href="/quote/start" passHref>
-          <button className="bg-gray-50 text-slate-800 max-w-[200px] text-center font-bold flex space-x-2 py-2 px-4 items-center mt-4 rounded-full">
+          <button className="bg-gray-50 text-slate-800 max-w-[200px] text-center font-bold flex space-x-2 py-2 px-4 items-center mt-6 rounded-full">
             Get Your Geyser
             <span className="text-lg ml-2 font-bold">&rarr;</span>
           </button>
