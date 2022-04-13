426f00d60659182c075bc567cd62711e22f9bff2
Philani Ncube <ncbphi001@gmail.com>
2022-04-13 07:13:05 +0200

    hero update

--- components/Homepage/Hero.jsx
+++ components/Homepage/Hero.jsx
@@ -19,14 +19,13 @@ const Hero = () => {
       </div>
       <div className="max-w-7xl flex flex-col space-y-3 justify-center aspect-video min-h-[70vh] mx-auto bg-cover z-20 bg-center bg-no-repeat px-4 lg:px-16 py-12">
         <h1 className="text-2xl lg:text-4xl font-sans font-bold mb-3 text-white">
-          Ave up to <span className="lg:text-6xl ">20%</span> <br />
-          on electricity bill.
+          Save up to <span className="lg:text-6xl ">20%</span> <br />
+          on your monthly electricity bill.
         </h1>
         <p className="text-md lg:text-xl mb-4 font-medium leading-3 text-white">
-          Say goodbye to cold showers, <br />
-          get a gas geyser installation
-          <br />
-          for your home.
+          Say goodbye to soaring electricity prices
+          <br /> and enjoy the comfort of a gas geyser <br />
+          installation for your home.
         </p>
         <Link href="/quote/start" passHref>
           <button className="bg-gray-50 text-slate-800 max-w-[200px] text-center font-bold flex space-x-2 py-2 px-4 items-center mt-6 rounded-full">
