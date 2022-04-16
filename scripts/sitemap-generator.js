834d75a8fcf8a69e58dd0f2c4160120b06ca0773
Philani Ncube <ncbphi001@gmail.com>
2022-04-16 20:36:10 +0200

    sitemap

--- scripts/sitemap-generator.js
+++ scripts/sitemap-generator.js
@@ -6,7 +6,7 @@ function addPage(page) {
     .replace('.jsx', '')
     .replace('.mdx', '');
   const route = path === '/index' ? '' : path;
-  return `  <url>
+  return `<url>
     <loc>${`${process.env.WEBSITE_URL}${route}`}</loc>
     <lastmod>${new Date().toISOString()}</lastmod>
     <changefreq>monthly</changefreq>
@@ -18,18 +18,21 @@ async function generateSitemap() {
   // excludes Nextjs files and API routes.
   const pages = await globby([
     'pages/**/*{.jsx,.mdx}',
+    'pages/blog/**/*{.jsx,.mdx}',
     'admin/**/*{.jsx,.mdx}',
     'orders/**/*{.jsx,.mdx}',
     '/index/**/*{.jsx,.mdx}',
     '!pages/api/**/*{.jsx,.mdx}',
-    '!pages/_*.jsx',
+    '!pages/_app{.jsx,.mdx}',
+    '!pages/_document{.jsx,.mdx}',
+    '!pages/index{.jsx,.mdx}',
     '!pages/profile/**/*{.jsx,.mdx}',
     '!pages/admin/**/*{.jsx,.mdx}',
+    '!pages/admin/products/index{.jsx,.mdx}',
     '!pages/orders/**/*{.jsx,.mdx}',
-    '!pages/quote/index{.jsx,.mdx}',
-    '!pages/profile/index{.jsx,.mdx}',
+    '!pages/orders/[id]{.jsx,.mdx}',
+    '!pages/blog/[slug]{.jsx,.mdx}',
     '!pages/catalogue/[id]{.jsx,.mdx}',
-    '!pages/catalogue/index{.jsx,.mdx}',
   ]);
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
