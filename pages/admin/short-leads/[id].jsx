7b7084b7896ce01703b252227d04093abbcdeb63
Philani Ncube <ncbphi001@gmail.com>
2022-05-19 18:06:10 +0200

    lead card

--- pages/admin/short-leads/[id].jsx
+++ pages/admin/short-leads/[id].jsx
@@ -12,21 +12,21 @@ function ShortLead({ lead }) {
       <div className="bg-white  shadow rounded xl:flex lg:flex max-w-6xl mx-auto px-6 md:px-4 lg:px-0 my-12">
         <div className="xl:w-2/5 lg:w-2/5 bg-gray-100 py-8 border-gray-300 xl:border-r rounded-tl xl:rounded-bl rounded-tr xl:rounded-tr-none lg:border-r-2 border-b xl:border-b-0 flex justify-center items-center">
           <div className="flex flex-col items-center">
-            <p className="mb-2 text-lg font-bold text-gray-900">Shane Doe</p>
-            <p className="mb-6 text-sm text-gray-700">shane@awesomeness.com</p>
-            <button className="bg-white font-medium transition duration-150 ease-in-out hover:bg-gray-200 rounded text-gray-800 px-6 py-2 text-sm border border-gray-300 focus:outline-none">
-              Send Message
-            </button>
+            <p className="mb-2 text-lg font-bold text-gray-900">{`${lead.firstName} ${lead.lastName}`}</p>
+            <p className="mb-6 text-sm text-gray-700">{lead.email}</p>
+            <div className="bg-white font-medium transition duration-150 ease-in-out hover:bg-gray-200 rounded text-gray-800 px-6 py-2 text-sm border border-gray-300 focus:outline-none">
+              Flow Rate {lead.flowRate} l/min
+            </div>
           </div>
         </div>
         <div className="xl:w-3/5 lg:w-3/5 px-6 py-8">
           <div className="flex flex-wrap justify-between">
             <div className="w-2/5 mb-8">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 font-medium">
-                  Birthday
+                <p className="mb-2 text-sm text-gray-700 font-medium">Date</p>
+                <p className="text-sm text-gray-700">
+                  {new Date(lead.created_at).toDateString()}
                 </p>
-                <p className="text-sm text-gray-700">29 Jan, 1982</p>
               </div>
             </div>
             <div className="w-2/5 mb-8">
@@ -40,7 +40,8 @@ function ShortLead({ lead }) {
                 <p className="mb-2 text-sm text-gray-700 font-medium">
                   Location
                 </p>
-                <p className="text-sm text-gray-700">California, USA</p>
+                <p className="text-sm text-gray-700">{`${lead.streetAddress}, ${lead.suburb}, 
+                ${lead.city} `}</p>
               </div>
             </div>
             <div className="w-2/5 mb-8">
@@ -48,21 +49,39 @@ function ShortLead({ lead }) {
                 <p className="mb-2 text-sm text-gray-700 font-medium">
                   Phone Number
                 </p>
-                <p className="text-sm text-gray-700">202-555-0191</p>
+                <p className="text-sm text-gray-700">{lead.phoneNumber}</p>
               </div>
             </div>
             <div className="w-2/5">
               <div className="border-b pb-3">
                 <p className="mb-2 text-sm text-gray-700 font-medium">
-                  Last Login
+                  Installation
                 </p>
-                <p className="text-sm text-gray-700 ">Today</p>
+                <p className="text-sm text-gray-700 ">
+                  {lead.installation ? 'Yes' : 'No'}
+                </p>
               </div>
             </div>
-            <div className="w-2/5">
-              <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 font-medium">Status</p>
-                <p className="text-sm text-gray-700 ">Approved</p>
+            <div className="w-2/5 ">
+              <div className="border-b pb-3 flex justify-between">
+                <span>
+                  <p className="mb-2 text-sm text-gray-700 font-medium">
+                    Sinks
+                  </p>
+                  <p className="text-sm text-gray-700 ">{lead.sinks}</p>
+                </span>
+                <span>
+                  <p className="mb-2 text-sm text-gray-700 font-medium">
+                    Bathtubs
+                  </p>
+                  <p className="text-sm text-gray-700 ">{lead.bathtubs}</p>
+                </span>
+                <span>
+                  <p className="mb-2 text-sm text-gray-700 font-medium">
+                    Showers
+                  </p>
+                  <p className="text-sm text-gray-700 ">{lead.showers}</p>
+                </span>
               </div>
             </div>
           </div>
