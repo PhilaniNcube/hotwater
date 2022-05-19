4c4f894c3c69e8b363214f8fe27f34f401a3a6d5
Philani Ncube <ncbphi001@gmail.com>
2022-05-19 17:52:59 +0200

    remove dark mode

--- pages/admin/short-leads/[id].jsx
+++ pages/admin/short-leads/[id].jsx
@@ -9,16 +9,12 @@ function ShortLead({ lead }) {
     <Fragment>
       {/* Card is full width. Use in 8 col grid for best view. */}
       {/* Card code block start */}
-      <div className="bg-white dark:bg-gray-800 shadow rounded xl:flex lg:flex w-full">
-        <div className="xl:w-2/5 lg:w-2/5 bg-gray-100 dark:bg-gray-800 py-8 border-gray-300 dark:border-gray-200 xl:border-r rounded-tl xl:rounded-bl rounded-tr xl:rounded-tr-none lg:border-r-2 border-b xl:border-b-0 flex justify-center items-center">
+      <div className="bg-white  shadow rounded xl:flex lg:flex max-w-6xl mx-auto px-6 md:px-4 lg:px-0 my-12">
+        <div className="xl:w-2/5 lg:w-2/5 bg-gray-100 py-8 border-gray-300 xl:border-r rounded-tl xl:rounded-bl rounded-tr xl:rounded-tr-none lg:border-r-2 border-b xl:border-b-0 flex justify-center items-center">
           <div className="flex flex-col items-center">
-            <p className="mb-2 text-lg font-bold text-gray-900 dark:text-gray-100">
-              Shane Doe
-            </p>
-            <p className="mb-6 text-sm text-gray-700 dark:text-gray-400">
-              shane@awesomeness.com
-            </p>
-            <button className="bg-white font-medium transition duration-150 ease-in-out hover:bg-gray-200 rounded text-gray-800 px-6 py-2 text-sm border border-gray-300 dark:border-gray-200 focus:outline-none">
+            <p className="mb-2 text-lg font-bold text-gray-900">Shane Doe</p>
+            <p className="mb-6 text-sm text-gray-700">shane@awesomeness.com</p>
+            <button className="bg-white font-medium transition duration-150 ease-in-out hover:bg-gray-200 rounded text-gray-800 px-6 py-2 text-sm border border-gray-300 focus:outline-none">
               Send Message
             </button>
           </div>
@@ -27,60 +23,46 @@ function ShortLead({ lead }) {
           <div className="flex flex-wrap justify-between">
             <div className="w-2/5 mb-8">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
+                <p className="mb-2 text-sm text-gray-700 font-medium">
                   Birthday
                 </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">
-                  29 Jan, 1982
-                </p>
+                <p className="text-sm text-gray-700">29 Jan, 1982</p>
               </div>
             </div>
             <div className="w-2/5 mb-8">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
-                  Gender
-                </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">Male</p>
+                <p className="mb-2 text-sm text-gray-700 font-medium">Gender</p>
+                <p className="text-sm text-gray-700">Male</p>
               </div>
             </div>
             <div className="w-2/5 mb-8">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
+                <p className="mb-2 text-sm text-gray-700 font-medium">
                   Location
                 </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">
-                  California, USA
-                </p>
+                <p className="text-sm text-gray-700">California, USA</p>
               </div>
             </div>
             <div className="w-2/5 mb-8">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
+                <p className="mb-2 text-sm text-gray-700 font-medium">
                   Phone Number
                 </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">
-                  202-555-0191
-                </p>
+                <p className="text-sm text-gray-700">202-555-0191</p>
               </div>
             </div>
             <div className="w-2/5">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
+                <p className="mb-2 text-sm text-gray-700 font-medium">
                   Last Login
                 </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">
-                  Today
-                </p>
+                <p className="text-sm text-gray-700 ">Today</p>
               </div>
             </div>
             <div className="w-2/5">
               <div className="border-b pb-3">
-                <p className="mb-2 text-sm text-gray-700 dark:text-gray-400 font-medium">
-                  Status
-                </p>
-                <p className="text-sm text-gray-700 dark:text-gray-400">
-                  Approved
-                </p>
+                <p className="mb-2 text-sm text-gray-700 font-medium">Status</p>
+                <p className="text-sm text-gray-700 ">Approved</p>
               </div>
             </div>
           </div>
@@ -109,7 +91,7 @@ export async function getServerSideProps({ req, params: { id } }) {
 
   let { data: leads, error } = await supabase
     .from('leads')
-    .select('created_at')
+    .select('*')
     .eq('id', id)
     .single();
 
