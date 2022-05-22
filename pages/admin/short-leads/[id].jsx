7c95a1c36f6f80d3d92d861129874c6a844a1891
Philani Ncube <ncbphi001@gmail.com>
2022-05-22 23:50:19 +0200

    short leads installer email

--- pages/admin/short-leads/[id].jsx
+++ pages/admin/short-leads/[id].jsx
@@ -1,9 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { supabase } from '../../../utils/supabase';
 import cookie from 'cookie';
 
 function ShortLead({ lead }) {
   console.log({ lead });
+
+  const [receipient, setReceipient] = useState('');
+  const [secondary, setSecondary] = useState('');
+  const [messages, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const mail = await fetch(`/api/mail/short-installers`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        showers: lead.showers,
+        bathtubs: lead.bathtub,
+        sinks: lead.sinks,
+        flowRate: lead.flowRate,
+        firstName: lead.firstName,
+        lastName: lead.lastName,
+        email: lead.email,
+        streetAddress: lead.streetAddress,
+        suburb: lead.suburb,
+        city: lead.city,
+        phoneNumber: lead.phoneNumber,
+        installation: lead.installation,
+        messages: messages,
+        receipient: receipient,
+        secondary: secondary,
+      }),
+    });
+
+    console.log(mail);
+
+    if (mail.status === 200) {
+      alert(`Email sent to ${receipient}`);
+    }
+  };
 
   return (
     <Fragment>
@@ -88,6 +125,74 @@ function ShortLead({ lead }) {
         </div>
       </div>
       {/* Card code block end */}
+
+      <form
+        className="md:w-1/2 mx-auto px-6 bg-gray-100 py-4 rounded my-8 lg:rounded-lg"
+        onSubmit={handleSubmit}
+      >
+        <p className="text-lg font-bold my-4">Send message to the installer</p>
+        <div className="flex flex-col ">
+          <label
+            htmlFor="receipient"
+            className="text-gray-800  text-sm font-bold leading-tight tracking-normal mb-2"
+          >
+            Email Address
+          </label>
+          <input
+            id="receipient"
+            required
+            type="email"
+            value={receipient}
+            onChange={(e) => setReceipient(e.target.value)}
+            className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700  bg-white font-normal w-64 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow"
+            placeholder="Placeholder"
+          />
+
+          <div className="flex flex-col mt-4">
+            <label
+              htmlFor="secondary"
+              className="text-gray-800  text-sm font-bold leading-tight tracking-normal mb-2"
+            >
+              CC:Email Address
+            </label>
+            <input
+              id="secondary"
+              required
+              type="email"
+              value={secondary}
+              onChange={(e) => setSecondary(e.target.value)}
+              className="text-gray-600 focus:outline-none focus:border focus:border-indigo-700  bg-white font-normal w-64 h-10 flex items-center pl-3 text-sm border-gray-300 rounded border shadow"
+              placeholder="Placeholder"
+            />
+          </div>
+
+          <div className="mt-8 flex flex-col xl:w-3/5 lg:w-1/2 md:w-1/2 w-full">
+            <label
+              htmlFor="message"
+              className="pb-2 text-sm font-bold text-gray-800"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              type="text"
+              name="message"
+              value={messages}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+              className="bg-white border border-gray-300 pl-3 py-3 shadow-sm rounded text-sm focus:outline-none focus:border-indigo-700 resize-none placeholder-gray-500 text-gray-500 "
+              placeholder="Short Message"
+              rows={5}
+            />
+          </div>
+          <button
+            disabled={loading}
+            className="mt-4 bg-sky-700 text-white py-2 rounded-md"
+          >
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+        </div>
+      </form>
     </Fragment>
   );
 }
