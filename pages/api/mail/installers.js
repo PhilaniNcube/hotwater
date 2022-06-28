47ad597808e5b4422df1af043b07c0efe6876907
Philani Ncube <ncbphi001@gmail.com>
2022-06-28 13:50:24 +0200

    bathroom Sink

--- pages/api/mail/installers.js
+++ pages/api/mail/installers.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
     rainShower,
     bathtub,
     kitchenSink,
-    bathroonSink,
+    bathroomSink,
     dishwasher,
     washingmachine,
     flowRate,
