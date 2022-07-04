77690879b9bb1707a1c6438d5fb75f4f3a7330a1
Philani Ncube <ncbphi001@gmail.com>
2022-07-04 21:19:52 +0200

    graph

--- components/QuoteSteps/Recommendations.jsx
+++ components/QuoteSteps/Recommendations.jsx
@@ -7,52 +7,39 @@ import * as d3 from 'd3';
 import formatter from '../../lib/format';
 import { parse } from 'cookie';
 import roundUp from '../../lib/roundUp';
+import { select, scaleBand, axisBottom, stack, max, scaleLinear, axisLeft } from 'd3';
 
 const Recommendations = ({ quoteInfo, nextPage, prevPage, page, setQuoteInfo }) => {
   const [ref, { height, width }] = useMeasure();
   console.log('Step9', quoteInfo);
 
-const [geyserPrice, setGeyserPrice] = useState();
-const [geyserSize, setGeyserSize] = useState();
+  const [geyserPrice, setGeyserPrice] = useState();
+  const [geyserSize, setGeyserSize] = useState();
 
-
-
-
-
-
-   const pieChart = useRef();
-
-   let radius = Math.min(width, height) / 2;
-
-
-  // const flowRate = Math.ceil(parseInt(quoteInfo.flowRate + 1));
-  // const topRate = Math.ceil(parseInt(quoteInfo.flowRate + 8.3));
-
-   let ups;
-   let min;
-   let max;
-
-    if(geyserSize < 20) {
-      ups = 0
-    } else {
-      ups = 2500 * 1.15
-    }
+  let installationMin;
+  let installationMax;
 
 
     if(quoteInfo.gasSupply === 'none') {
 
+      installationMin = ((5000*1.15) + (1800*1.15));
+      installationMax = ((5000*1.15) + (3500*1.15));
 
-       min = ((5000*1.15) + (1800*1.15) + (2500*1.15));
-      max = ((8000*1.15) + (3500*1.15) + (4000*1.15) );
+
 
 
     } else {
-      min = ((5000*1.15) +  (2500*1.15) );
-      max = ((8000*1.15) +  (4000*1.15) );
+      installationMin = ((5000*1.15) );
+      installationMax = ((8000*1.15) );
+
     }
 
 
+  const svgRef = useRef()
 
+  const keys = ['GeyserPrice', 'Installation', 'Plumbing']
+
+  const colors = {"GeyserPrice":"#345ac0", "Installation":"#a4b0a0", "Plumbing":"#c0ba0c"}
 
 
 
@@ -80,85 +67,60 @@ const [geyserSize, setGeyserSize] = useState();
 
   },[]);
 
+     let data = [
+    {range: 'Minimum Cost', GeyserPrice: geyserPrice, Installation: installationMin, Plumbing: 2500*1.15 },
+    {range: 'Maximum Cost', GeyserPrice: geyserPrice, Installation: installationMax, Plumbing: 4000*1.15 },
+  ]
+
+  useEffect(() => {
+    const svg = select(svgRef.current)
+
+    const stackGenerator = stack().keys(keys)
+    const layers = stackGenerator(data)
+
+    const extent = [0, max(layers, layer => max(layer, sequence => sequence[1]))]
+    console.log({extent})
+
+    const xScale = scaleBand().domain(data.map(d =>  d.range)).range([0, width*.9]).padding(0.4)
+
+    const xAxis = axisBottom(xScale)
+    svg.select(".x-axis").attr("transform", `translate(0, ${height * 0.8})`).call(xAxis)
+
+    const yScale = scaleLinear().domain(extent).range([(height *0.8) , 0])
+
+    const yAxis = axisLeft(yScale)
+    svg.select(".y-axis").call(yAxis)
+
+    svg.selectAll(".layer").data(layers).join("g").attr("class", "layer").attr("fill", layer => {
+      return colors[layer.key]
+    })
+    .selectAll("rect").data(layer => layer).join("rect").attr("x", sequence => {
+      return xScale(sequence.data.range)
+    }).attr("width", xScale.bandwidth()).attr("y", sequence => yScale(sequence[1]))
+    .attr("height", sequence => yScale(sequence[0]) - yScale(sequence[1]))
 
 
+    console.log({height})
 
+
+   }, [colors, installationMin, installationMax, width, height])
 
 
   return (
-    <div className="mt-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 max-w-6xl px-4 mx-auto">
-         <div className="md:border-r md:border-gray-300 w-full">
-            <p className="text-gray-800 font-bold text-3xl text-center">Your Geyser</p>
+    <div className="mt-8 min-h-[90vh]">
 
-            {quoteInfo.flowRate < 26 ? (
-              <div>
-                  <p className=" text-gray-700 font-bold text-sm md:text-base">Based on your answers, you need a gas geyser with the following flow rate</p>
-                  <p className="font-bold text-sky-500 text-5xl text-center flex flex-col">
-                    <span>{geyserSize}l/min</span>
-                    <span className="text-lg">flow rate</span>
-                  </p>
-
-                  <p className="text-center text-gray-700 font-bold text-sm mt-3 md:text-base">You are looking at:</p>
-                   <p className="font-bold text-sky-500 text-5xl text-center flex flex-col">{formatter.format(parseInt(geyserPrice))}</p>
-                    <p className="text-center text-gray-700 font-bold text-sm mt-3 md:text-base">for the geyser</p>
-                </div>
-              ) : (
-                <article>
-                <p className="text-sm text-gray-700">Based on your answers, we have calculated that you will need a geysers exceeding a flow rate of <span className="font-bold">26 L/min.</span> This will require a specialised installation. Please complete the following questions so that we can get in touch a suitable solution. </p>
-                </article>
-              )}
-
-
-              <div className="flex w-full flex-col items-center space-y-3 md:flex-row md:justify-center md:space-x-3 md:space-y-0 mt-4">
-              <span className="flex py-3 px-6 cursor-pointer rounded-full items-center space-x-3 text-white font-bold bg-sky-500 text-xl">
-                 <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                 </svg>
-                 <p>Yes please</p>
-              </span>
-              <span className="flex py-3 px-6 rounded-full cursor-pointer space-x-3 items-center text-white font-bold bg-red-500 text-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                 <p>No thanks</p>
-              </span>
-              </div>
-
-         </div>
-
-         <div className="md:border-l flex flex-col mt-10 md:mt-0 items-center  md:border-gray-300 w-full px-8">
-             <p className="text-gray-800 font-bold text-3xl text-center">Installation & Plumbing</p>
-              <p className=" text-gray-700 font-bold text-sm text-center md:text-base">Based on the flowrate of your recommended geyser, your installation will be in the range of:</p>
-                  <p className="font-bold text-sky-500 text-center mt-3 flex flex-col">
-
-                    <span className="text-xl">{`${formatter.format((roundUp(min)))} - ${formatter.format(roundUp(max))}`}</span>
-                  </p>
-
-                   <p className=" text-gray-700 font-bold text-sm text-center md:text-base mt-8">Are you interested in getting a certified technician to install your gas geyser?</p>
-
-                    <div className="flex flex-1  w-full flex-col items-center space-y-3 md:flex-row md:justify-center md:items-end md:space-x-3 md:space-y-0 mt-4">
-              <span onClick={() => {
-                setQuoteInfo({...quoteInfo, installation: true })
-              }}
-              className="flex py-3 px-6 cursor-pointer rounded-full items-center space-x-3 text-white font-bold bg-sky-500 text-xl">
-                 <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                 </svg>
-                 <p>Yes please</p>
-              </span>
-              <span onClick={() => {
-                setQuoteInfo({...quoteInfo, installation: false })
-              }} className="flex py-3 px-6 rounded-full cursor-pointer space-x-3 items-center text-white font-bold bg-red-500 text-xl">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                 <p>No thanks</p>
-              </span>
-              </div>
-         </div>
+    <p className="text-center text-4xl text-gray-700 mb-3 font-bold">Geyser Size: {geyserSize}L/Min</p>
+    <h2 className="text-center text-4xl text-gray-700  font-bold">Estimated Cost</h2>
+    <div className="flex max-w-7xl px-6 min-h-[500px] mx-auto flex-col bg-gray-100 py-3 mb-8 "  ref={ref} >
+     <svg ref={svgRef} className="w-full mx-auto translate-x-3 h-[500px] ">
+       <g className="x-axis" />
+       <g className="y-axis" />
+       </svg>
       </div>
-       <div className="flex items-center justify-center space-x-6 mt-12 mb-8">
+
+
+
+       <div className="flex items-center justify-center space-x-6 mt-6 mb-8">
         {quoteInfo.installation !== null ?
         (
           <Fragment>
