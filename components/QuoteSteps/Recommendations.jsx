b643b8e93642f2f5a788c90811d67e085c541ba5
Philani Ncube <ncbphi001@gmail.com>
2022-07-09 15:44:02 +0200

    swapped button

--- components/QuoteSteps/Recommendations.jsx
+++ components/QuoteSteps/Recommendations.jsx
@@ -182,37 +182,6 @@ const Recommendations = ({
                     onClick={() => {
                       setQuoteInfo({
                         ...quoteInfo,
-                        installation: "No thanks",
-                        installationCost: installation,
-                        plumbingCost: plumbing,
-                        geyserPrice: geyserPrice,
-                        geyserSize: geyserSize,
-                      });
-                      nextPage();
-                    }}
-                    className="flex items-center bg-red-500 cursor-pointer rounded-full px-6 py-2 space-x-2 text-white text-base font-medium "
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-10"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      />
-                    </svg>
-                    <p>No thanks</p>
-                  </span>
-
-                  <span
-                    onClick={() => {
-                      setQuoteInfo({
-                        ...quoteInfo,
                         installation: "full installation",
                         installationCost: installation,
                         plumbingCost: plumbing,
@@ -265,6 +234,37 @@ const Recommendations = ({
                       />
                     </svg>
                     <p>Gas geyser only quote</p>
+                  </span>
+
+                  <span
+                    onClick={() => {
+                      setQuoteInfo({
+                        ...quoteInfo,
+                        installation: "No thanks",
+                        installationCost: installation,
+                        plumbingCost: plumbing,
+                        geyserPrice: geyserPrice,
+                        geyserSize: geyserSize,
+                      });
+                      nextPage();
+                    }}
+                    className="flex items-center bg-red-500 cursor-pointer rounded-full px-6 py-2 space-x-2 text-white text-base font-medium "
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-10 w-10"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                    <p>No thanks</p>
                   </span>
                 </div>
               </Fragment>
