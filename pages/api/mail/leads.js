a2df753145900e4ee57f889dba33a3cd8ed1e9b8
Philani Ncube <ncbphi001@gmail.com>
2022-07-09 19:22:16 +0200

    new email

--- pages/api/mail/leads.js
+++ pages/api/mail/leads.js
@@ -61,132 +61,9 @@ export default async function handler(req, res) {
     subject: "Hotwater24 | Your size gas geyser",
     text: `Thank you for answering questions about your property and gas geyser needs. We will be in touch shortly`,
     html: `
-<html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-      <!--[if !mso]><!-->
-      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-      <!--<![endif]-->
-      <!--[if (gte mso 9)|(IE)]>
-      <xml>
-        <o:OfficeDocumentSettings>
-          <o:AllowPNG/>
-          <o:PixelsPerInch>96</o:PixelsPerInch>
-        </o:OfficeDocumentSettings>
-      </xml>
-      <![endif]-->
-      <!--[if (gte mso 9)|(IE)]>
-  <style type="text/css">
-    body {width: 600px;margin: 0 auto;}
-    table {border-collapse: collapse;}
-    table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
-    img {-ms-interpolation-mode: bicubic;}
-  </style>
-<![endif]-->
-      <style type="text/css">
-    body, p, div {
-      font-family: inherit;
-      font-size: 14px;
-    }
-    body {
-      color: #000000;
-    }
-    body a {
-      color: #1188E6;
-      text-decoration: none;
-    }
-    p { margin: 0; padding: 0; }
-    table.wrapper {
-      width:100% !important;
-      table-layout: fixed;
-      -webkit-font-smoothing: antialiased;
-      -webkit-text-size-adjust: 100%;
-      -moz-text-size-adjust: 100%;
-      -ms-text-size-adjust: 100%;
-    }
-    img.max-width {
-      max-width: 100% !important;
-    }
-    .column.of-2 {
-      width: 50%;
-    }
-    .column.of-3 {
-      width: 33.333%;
-    }
-    .column.of-4 {
-      width: 25%;
-    }
-    ul ul ul ul  {
-      list-style-type: disc !important;
-    }
-    ol ol {
-      list-style-type: lower-roman !important;
-    }
-    ol ol ol {
-      list-style-type: lower-latin !important;
-    }
-    ol ol ol ol {
-      list-style-type: decimal !important;
-    }
-    @media screen and (max-width:480px) {
-      .preheader .rightColumnContent,
-      .footer .rightColumnContent {
-        text-align: left !important;
-      }
-      .preheader .rightColumnContent div,
-      .preheader .rightColumnContent span,
-      .footer .rightColumnContent div,
-      .footer .rightColumnContent span {
-        text-align: left !important;
-      }
-      .preheader .rightColumnContent,
-      .preheader .leftColumnContent {
-        font-size: 80% !important;
-        padding: 5px 0;
-      }
-      table.wrapper-mobile {
-        width: 100% !important;
-        table-layout: fixed;
-      }
-      img.max-width {
-        height: auto !important;
-        max-width: 100% !important;
-      }
-      a.bulletproof-button {
-        display: block !important;
-        width: auto !important;
-        font-size: 80%;
-        padding-left: 0 !important;
-        padding-right: 0 !important;
-      }
-      .columns {
-        width: 100% !important;
-      }
-      .column {
-        display: block !important;
-        width: 100% !important;
-        padding-left: 0 !important;
-        padding-right: 0 !important;
-        margin-left: 0 !important;
-        margin-right: 0 !important;
-      }
-      .social-icon-column {
-        display: inline-block !important;
-      }
-    }
-  </style>
-    <style>
-      @media screen and (max-width:480px) {
-        table\0 {
-          width: 480px !important;
-          }
-      }
-    </style>
-      <!--user entered Head Start--><link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet"><style>
-    body {font-family: 'Viga', sans-serif;}
-</style><!--End Head user entered-->
-    </head>
+
+
+
     <body>
       <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:inherit; color:#000000; background-color:#f0f0f0;">
         <div class="webkit">
@@ -235,8 +112,8 @@ export default async function handler(req, res) {
     <tbody>
       <tr>
         <td style="padding:18px 20px 18px 30px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">${firstName} ${lastName}</div>
-<div style="font-family: inherit; text-align: inherit">${streetAddress}, ${suburb}, ${postalCode}</div>
-<div style="font-family: inherit; text-align: inherit">${phoneNumber}</div>
+<div style="font-family: inherit; text-align: inherit">${streetAddress}, ${suburb}, ${postalCode}, ${city}</div>
+<div style="font-family: inherit; text-align: inherit">${telephoneNumber}</div>
 <div style="font-family: inherit; text-align: inherit">${email}</div><div></div></div></td>
       </tr>
     </tbody>
@@ -381,7 +258,9 @@ export default async function handler(req, res) {
       <tr>
         <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Electric</div>
 <div style="font-family: inherit; text-align: inherit"><br></div>
-<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${electricGeyser}</span></div><div></div></div></td>
+<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${
+      electricGeyser ? "Yes" : "No"
+    }</span></div><div></div></div></td>
       </tr>
     </tbody>
   </table></td>
@@ -395,7 +274,9 @@ export default async function handler(req, res) {
       <tr>
         <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Solar</div>
 <div style="font-family: inherit; text-align: inherit"><br></div>
-<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${solarGeyser}</span></div><div></div></div></td>
+<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${
+      solarGeyser ? "Yes" : "No"
+    }</span></div><div></div></div></td>
       </tr>
     </tbody>
   </table></td>
@@ -409,7 +290,9 @@ export default async function handler(req, res) {
       <tr>
         <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Gas Geyser</div>
 <div style="font-family: inherit; text-align: inherit"><br></div>
-<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${gasGeyser}</span></div><div></div></div></td>
+<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${
+      gasGeyser ? "Yes" : "No"
+    }</span></div><div></div></div></td>
       </tr>
     </tbody>
   </table></td>
@@ -423,7 +306,9 @@ export default async function handler(req, res) {
       <tr>
         <td style="padding:18px 0px 18px 0px; line-height:22px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Other</div>
 <div style="font-family: inherit; text-align: inherit"><br></div>
-<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${otherGeyser}</span></div><div></div></div></td>
+<div style="font-family: inherit; text-align: inherit"><span style="color: #0055ff">${
+      otherGeyser ? "Yes" : "No"
+    }</span></div><div></div></div></td>
       </tr>
     </tbody>
   </table></td>
@@ -449,10 +334,15 @@ export default async function handler(req, res) {
   </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="264ee24b-c2b0-457c-a9c1-d465879f9935.1" data-mc-module-version="2019-10-22">
     <tbody>
       <tr>
-        <td style="padding:18px 20px 30px 30px; line-height:22px; text-align:inherit; background-color:#FFFFFF;" height="100%" valign="top" bgcolor="#FFFFFF" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Flow Rate - ${flowRate}</div>
-<div style="font-family: inherit; text-align: inherit">Geyser Size - ${geyserSize}</div>
-<div style="font-family: inherit; text-align: inherit">Monthly Electricity Savings - ${monthlySavings}</div>
-<div style="font-family: inherit; text-align: inherit">Yearly Savings - ${yearlySavings}</div>
+        <td style="padding:18px 20px 30px 30px; line-height:22px; text-align:inherit; background-color:#FFFFFF;" height="100%" valign="top" bgcolor="#FFFFFF" role="module-content"><div><div style="font-family: inherit; text-align: inherit">Flow Rate - ${flowRate} L/min</div>
+<div style="font-family: inherit; text-align: inherit">Geyser Size - ${geyserSize ? geyserSize   : 'N/A'} L/Min</div>
+<div style="font-family: inherit; text-align: inherit">Geyser Size - ${installation ? installation : 'N/A'}</div>
+<div style="font-family: inherit; text-align: inherit">Monthly Electricity Savings - ${roundUp(
+      monthlySavings
+    )}</div>
+<div style="font-family: inherit; text-align: inherit">Yearly Savings - ${roundUp(
+      yearlySavings
+    )}</div>
 <div style="font-family: inherit; text-align: inherit"><br>
 <span style="font-size: 18px"><strong>Estimated Cost</strong></span></div>
 <div style="font-family: inherit; text-align: inherit"><br></div>
@@ -481,8 +371,117 @@ export default async function handler(req, res) {
           </table>
         </div>
       </center>
+        <style type="text/css">
+    body {width: 600px;margin: 0 auto;}
+    table {border-collapse: collapse;}
+    table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
+    img {-ms-interpolation-mode: bicubic;}
+  </style>
+<![endif]-->
+      <style type="text/css">
+    body, p, div {
+      font-family: inherit;
+      font-size: 14px;
+    }
+    body {
+      color: #000000;
+    }
+    body a {
+      color: #1188E6;
+      text-decoration: none;
+    }
+    p { margin: 0; padding: 0; }
+    table.wrapper {
+      width:100% !important;
+      table-layout: fixed;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-size-adjust: 100%;
+      -moz-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+    img.max-width {
+      max-width: 100% !important;
+    }
+    .column.of-2 {
+      width: 50%;
+    }
+    .column.of-3 {
+      width: 33.333%;
+    }
+    .column.of-4 {
+      width: 25%;
+    }
+    ul ul ul ul  {
+      list-style-type: disc !important;
+    }
+    ol ol {
+      list-style-type: lower-roman !important;
+    }
+    ol ol ol {
+      list-style-type: lower-latin !important;
+    }
+    ol ol ol ol {
+      list-style-type: decimal !important;
+    }
+    @media screen and (max-width:480px) {
+      .preheader .rightColumnContent,
+      .footer .rightColumnContent {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent div,
+      .preheader .rightColumnContent span,
+      .footer .rightColumnContent div,
+      .footer .rightColumnContent span {
+        text-align: left !important;
+      }
+      .preheader .rightColumnContent,
+      .preheader .leftColumnContent {
+        font-size: 80% !important;
+        padding: 5px 0;
+      }
+      table.wrapper-mobile {
+        width: 100% !important;
+        table-layout: fixed;
+      }
+      img.max-width {
+        height: auto !important;
+        max-width: 100% !important;
+      }
+      a.bulletproof-button {
+        display: block !important;
+        width: auto !important;
+        font-size: 80%;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+      }
+      .columns {
+        width: 100% !important;
+      }
+      .column {
+        display: block !important;
+        width: 100% !important;
+        padding-left: 0 !important;
+        padding-right: 0 !important;
+        margin-left: 0 !important;
+        margin-right: 0 !important;
+      }
+      .social-icon-column {
+        display: inline-block !important;
+      }
+    }
+  </style>
+    <style>
+      @media screen and (max-width:480px) {
+        table\0 {
+          width: 480px !important;
+          }
+      }
+    </style>
+      <!--user entered Head Start--><link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet"><style>
+    body {font-family: 'Viga', sans-serif;}
+</style><!--End Head user entered-->
     </body>
-  </html>
+
            `,
   };
 
