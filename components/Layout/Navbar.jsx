97d077ad60c7e6131794e68936097eca3f1c1d28
Philani Ncube <ncbphi001@gmail.com>
2022-09-16 18:25:46 +0200

    hide search

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -53,38 +53,8 @@ const Navbar = () => {
             </div>
           </Link>
           {/*Search Input*/}
-          <form
-            onSubmit={handleSearch}
-            className="hidden md:flex flex-col flex-1 px-16"
-          >
-            <div className="relative w-full">
-              <div className="absolute text-gray-600  flex items-center pl-3 h-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-search"
-                  width={16}
-                  height={16}
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" />
-                  <circle cx={10} cy={10} r={7} />
-                  <line x1={21} y1={21} x2={15} y2={15} />
-                </svg>
-              </div>
-              <input
-                id="search"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                className="text-gray-600 rounded-full w-full bg-gray-200  focus:outline-none focus:border focus:border-gray-700 font-normal pr-20 sm:pr-32 h-10 flex items-center pl-10 text-sm border-gray-300 border shadow"
-                placeholder="Search products..."
-              />
-            </div>
-          </form>
+
+
 
           {/*Quote Button */}
           <div className="flex items-center">
