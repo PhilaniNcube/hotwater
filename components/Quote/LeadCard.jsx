c79304cf17f6ddd9e23e488d400f0bc5a41ef00f
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2022-10-03 14:08:40 +0200

    lead card address

--- components/Quote/LeadCard.jsx
+++ components/Quote/LeadCard.jsx
@@ -144,13 +144,13 @@ function LeadCard({ quote }) {
                   Address:
                 </h3>
                 <p className="text-gray-600  text-sm font-normal">
-                  {/**  `Street Address: ${quote.streetAddress}` */}
+                  { `Street Address: ${quote.streetAddress}`}
                 </p>
                 <p className="text-gray-600  text-sm font-normal">
-                  {/*`Suburb: ${quote.suburb}`  */}
+                  {`Suburb: ${quote.suburb}`}
                 </p>
                 <p className="text-gray-600  text-sm font-normal">
-                  {/*`Postal Code: ${quote.postalCode}`*/}
+                  {`Postal Code: ${quote.postalCode}`}
                 </p>
                 <p className="text-gray-600  text-sm font-normal">
                   {`City: ${quote.city}`}
