17119e3e0dd758cb62f64771e6bef09c64990eb9
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-01 15:33:12 +0200

    terms and conditions

---
+++ pages/terms.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const terms = () => {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-xl">TERMS AND CONDITIONS</h1>
+      <p className="text-md">
+        Welcome to Hotwater24.com. By using our website, you agree to be bound
+        by the following terms and conditions. Please read these terms and
+        conditions carefully before using our website.
+      </p>
+      <h2 className="text-lg">Use of Our Website</h2>
+      <p className="text-md">
+        a. You may use our website for lawful purposes only.
+      </p>
+      <p className="text-md">b. You may not use our website:</p>
+      <p className="text-sm">
+        i. In any way that breaches any applicable local, national, or
+        international law or regulation.
+      </p>
+      <p className="text-sm">
+        ii. In any way that is unlawful or fraudulent or has any unlawful or
+        fraudulent purpose or effect.
+      </p>
+      <p className="text-sm">
+        iii. To send, knowingly receive, upload, download, use, or re-use any
+        material that does not comply with our content standards.
+      </p>{" "}
+      <p className="text-md">
+        c. You may not use our website to transmit, or procure the sending of,
+        any unsolicited or unauthorized advertising or promotional material or
+        any other form of similar solicitation (spam).
+      </p>
+      <p className="text-md">
+        d. We reserve the right to terminate or restrict your use of our website
+        for any reason without notice or liability.
+      </p>
+      <h2 className="text-lg">Our Liability</h2>{" "}
+      <p className="text-md">
+        We do not guarantee that our website will be secure or free from bugs or
+        viruses.
+      </p>
+      <p className="text-md">
+        We will not be liable for any loss or damage caused by a virus,
+        distributed denial-of-service attack, or other technologically harmful
+        material that may infect your computer equipment, computer programs,
+        data, or other proprietary material due to your use of our website or to
+        your downloading of any material posted on it, or on any website linked
+        to it.
+      </p>
+      <p className="text-md">
+        c. We will not be liable for any loss or damage caused by any
+        unauthorized access to your personal information.
+      </p>
+    </div>
+  );
+}
+
+export default terms
