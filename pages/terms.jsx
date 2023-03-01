aa392bacc7b6ca7b5c8060ebe7783508c1779273
Philani Ncube <38924116+PhilaniNcube@users.noreply.github.com>
2023-03-01 16:44:56 +0200

    terms

--- pages/terms.jsx
+++ pages/terms.jsx
@@ -5,7 +5,7 @@ const terms = () => {
     <div className="max-w-7xl mx-auto py-20">
       <h1 className="text-xl">TERMS AND CONDITIONS</h1>
       <p className="text-md">
-        (Your Company) is a business in the gas water heater installation
+        Hotwater24 is a business in the gas water heater installation
         industry that supplies and installs gas water heaters.
       </p>{" "}
       <h2 className="text-lg">Delivery Policy</h2>
@@ -16,14 +16,14 @@ const terms = () => {
       </p>
       <h2 className="text-lg">Return and Refunds policy</h2>
       <p className="text-md">
-        The provision of goods and services by (Your Company) is subject to
-        availability. In cases of unavailability, (Your Company) will refund the
+        The provision of goods and services by Hotwater24 is subject to
+        availability. In cases of unavailability, Hotwater24 will refund the
         client in full within 30 days. Cancellation of orders by the client will
         attract a 5% administration fee.
       </p>
       <h2 className="text-lg">Customer Privacy policy</h2>
       <p className="text-md">
-        (Your Company) shall take all reasonable steps to protect the personal
+        Hotwater24 shall take all reasonable steps to protect the personal
         information of users. For the purpose of this clause, "personal
         information" shall be defined as detailed in the Promotion of Access to
         Information Act 2 of 2000 (PAIA). The PAIA may be downloaded from:
@@ -33,7 +33,7 @@ const terms = () => {
       <p className="text-md">Payment may be made via Visa and MasterCard.</p>
       <h2 className="text-lg">Card acquiring and security</h2>
       <p className="text-md">
-        Card transactions will be acquired for (Your Company) via PayGate (Pty)
+        Card transactions will be acquired for Hotwater24 via PayGate (Pty)
         Ltd who are the approved payment gateway for all South African Acquiring
         Banks. PayGate uses the strictest form of encryption, namely Secure
         Socket Layer 3 (SSL3) and no Card details are stored on the website.
@@ -42,7 +42,7 @@ const terms = () => {
       </p>
       <h2 className="text-lg">Customer details separate from card details</h2>
       <p className="text-md">
-        Customer details will be stored by (Your Company) separately from card
+        Customer details will be stored by Hotwater24 separately from card
         details which are entered by the client on PayGate’s secure site. For
         more detail on PayGate refer to www.paygate.co.za.
       </p>
@@ -56,30 +56,30 @@ const terms = () => {
       </p>
       <h2 className="text-lg">Responsibility</h2>
       <p className="text-md">
-        (Your Company) takes responsibility for all aspects relating to the
+        Hotwater24 takes responsibility for all aspects relating to the
         transaction including sale of goods and services sold on this website,
         customer service and support, dispute resolution and delivery of goods.
       </p>
       <h2 className="text-lg">Country of domicile</h2>
       <p className="text-md">
-        This website is governed by the laws of South Africa and (Your Company)
+        This website is governed by the laws of South Africa and Hotwater24
         chooses as its domicilium citandi et executandi for all purposes under
         this agreement, whether in respect of court process, notice, or other
-        documents or communication of whatsoever nature (Company Address).
+        documents or communication of whatsoever nature.
       </p>
       <h2 className="text-lg">Variation</h2>
       <p className="text-md">
-        (Your Company) may, in its sole discretion, change this agreement or any
+        Hotwater24 may, in its sole discretion, change this agreement or any
         part thereof at any time without notice.
       </p>
       <h2 className="text-lg">Company information</h2>
       <p className="text-md">
-        This website is run by Hotwater24 (Pty) Ltd ( private company) based in
+        This website is run by Hotwater24 (Pty) Ltd (private company) based in
         South Africa trading as Hotwater24 and with registration number
         2022/252320/07 and 2 (Director(s)).
       </p>
-      <h2 className="text-lg">(Your Company) contact details </h2>
-      <p className="text-md">Company Physical Address: 9 Kramer Road, Kramerville, Sandton, 2190. Email: info@hotwater24.com</p>
+      <h2 className="text-lg">Hotwater24 contact details </h2>
+      <p className="text-md">Company Physical Address: 9 Kramer Road, Kramerville, Sandton, 2190. Email: info@hotwater24.com Telephone: +27(0)79 341 4075</p>
     </div>
   );
 }
